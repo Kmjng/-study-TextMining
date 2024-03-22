d5d7cee738ea93f7faa6147bb3ecfcc11ee04f06
Minjeong Kim <ppz7227@naver.com>
2024-03-22 17:54:32 +0900

    Update exam04.py

--- chap02_Crawling/exams/exam04.py
+++ chap02_Crawling/exams/exam04.py
@@ -7,13 +7,33 @@
    <조건3> 크롤링 결과 확인  : news 개수와  news 출력  
 '''
 
-import urllib.request as req  # url 가져오기 
+from urllib.request import urlopen  # url 가져오기 
 from bs4 import BeautifulSoup
 
 
-# 클로러 함수(페이지수, 검색날짜) 
+
+
+# 크롤러 함수(페이지수, 검색날짜) 
 def crawler_func(pages, date):
-    pass # 내용 채우기    
+    news_list =[]
+    for page in range(1,pages+1): # 1~ 5
+        url = f'https://news.daum.net/newsbox?regDate={date}&tab_cate=NE&page={page}'
+        req = urlopen(url) 
+        read_req = req.read()  # txt 로 읽어오기
+    
+        decod_req = read_req.decode('utf-8')
+        
+        html = BeautifulSoup(decod_req,'html.parser')
+        lis = html.select('ul.list_arrange > li') # li 태그 요소들
+        #lis = html.select('ul[class="list_arrange"] > li > strong > a[class="link_txt"]') 
+        
+        
+        for li in lis:
+            a = li.find('a')
+            b = str(a.text).strip() 
+            # text로 요소들만 반환
+            news_list.append(b)
+    return news_list
 
 
 
@@ -21,6 +41,34 @@ def crawler_func(pages, date):
 
 # 클로러 함수 호출 
 crawling_news = crawler_func(5, '20220505') # (페이지수, 검색날짜)
+print(crawling_news)
+print(len(crawling_news)) # 200
 
+'''
+['아이들, 가장 많이 다친 장소 어딜까?', 
+ '"횡단보도도 없이 등교"..어린이가 위험하다', 
+ '"반갑다! 마스크 없는 어린이날"..동심 모처럼 활짝',....]
+'''
 
+# 단어들 담기 (불용어 처리 preprocessing해보자 )
+import re
+words =[] 
+for conts in crawling_news:
+    conts = re.sub('[^\s\d\w]','',conts)
+    for word in conts.split():
+        words.append(word)
+
+print(words)
+
+# 단어들 카운트 
+wc ={} 
+for i in words:
+    wc[i]=wc.get(i,0)+1
+
+wc= sorted(wc.items(), key = lambda x: x[1], reverse = True) # value(빈도수) 내림차순 정렬 
+print(wc)
+'''
+[('어린이날', 12), ('러', 8), ('美', 7), ('만에', 7), 
+ ('코로나', 7), ('우크라', 6), ('마스크', 5), ....]
+'''
 
