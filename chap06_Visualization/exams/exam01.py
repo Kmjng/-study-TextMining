d97a8aba019f80e2445c5d367e01b6aae0377096
Minjeong Kim <ppz7227@naver.com>
2024-03-26 17:43:05 +0900

    Create exam01.py

---
+++ chap06_Visualization/exams/exam01.py
@@ -0,0 +1,36 @@
+'''
+문1) 그리프 자료를 이용하여 다음 조건에 맞게 각 그래프를 그리시오.
+    <조건1> 막대 그리프 그리기 : categories, values, colors 변수 이용 
+    <조건2> 원 그래프 그리기 : values, colors 변수 이용 
+    <조건3> 누적형 가로막대 그래프 그리기 : categories, values, values2, colors 변수 이용
+    
+    <시각화 결과> exam01.pdf 참고  
+'''
+
+import matplotlib.pyplot as plt
+
+# 그래프 자료 
+categories = ['A', 'B', 'C', 'D']
+values = [15, 8, 12, 9]
+values2 = [4, 12, 5, 7]
+colors = ['blue', 'green', 'red', 'purple']  # 막대의 색상 리스트
+
+# <조건1> 막대 그래프 그리기 
+plt.bar(categories, values )
+plt.xlabel('카테고리')
+plt.ylabel('값')
+plt.show()
+
+# <조건2> 원 그래프 그리기 
+plt.pie(values, labels = colors)
+plt.show()
+
+# <조건3> 누적형 가로막대 그래프 그리기 
+plt.bar(categories, values, label = '데이터셋1', alpha = 0.5)
+plt.bar(categories, values2, bottom = values, label = '데이터셋2', alpha = 1)
+plt.xlabel('카테고리')
+plt.ylabel('값')
+plt.legend() 
+plt.show()
+
+
