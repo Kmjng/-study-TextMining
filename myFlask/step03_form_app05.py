3f6675676f103b81869a04c3c0923715e5a0fde0
Minjeong Kim <ppz7227@naver.com>
2024-04-04 17:34:30 +0900

    Create step03_form_app05.py

---
+++ myFlask/step03_form_app05.py
@@ -0,0 +1,33 @@
+'''
+템플릿 페이지에서 GET과 POST 동시에 전송되는 자료 받기
+'''
+
+from flask import Flask, render_template, request # request import
+
+app = Flask(__name__)
+
+
+@app.route('/') # 시작 페이지
+def index() :
+    return render_template('/step03/main.html') # 템플릿 파일
+   
+
+@app.route('/receive', methods=['POST']) # post 방식 
+def receive() :    
+    if request.method=='POST' : # post 방식 
+        name = request.form['uname'] # 파라미터 받기  
+        age = request.form['age']
+        
+        result = "[post 방식] 사용자 정보 : " + name + ', ' + age         
+        return render_template('/step03/result.html', result=result)
+
+    
+if __name__ == '__main__':
+    app.run(port=80)
+
+
+
+
+
+
+
