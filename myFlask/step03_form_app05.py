71d20168a80794f2e309d7cc5ce4bb278c9a6754
Minjeong Kim <ppz7227@naver.com>
2024-04-05 16:02:37 +0900

    Update step03_form_app05.py

--- myFlask/step03_form_app05.py
+++ myFlask/step03_form_app05.py
@@ -12,15 +12,18 @@ def index() :
     return render_template('/step03/main.html') # 템플릿 파일
    
 
-@app.route('/receive', methods=['POST']) # post 방식 
+@app.route('/receive', methods=['GET','POST']) # get, post 방식 
 def receive() :    
     if request.method=='POST' : # post 방식 
         name = request.form['uname'] # 파라미터 받기  
         age = request.form['age']
-        
-        result = "[post 방식] 사용자 정보 : " + name + ', ' + age         
-        return render_template('/step03/result.html', result=result)
-
+        results = "[post 방식] 사용자 정보 : " + name + ', ' + age         
+    if request.method == 'GET':  # get 방식 
+        name = request.args.get('uname')
+        age = request.args.get('age')
+        results = "[GET 방식] 특정사용자 정보: "+ name+','+age
+        # 여기서 name, age는 url_for의 name, age
+    return render_template('step03/result.html',result= results)
     
 if __name__ == '__main__':
     app.run(port=80)
